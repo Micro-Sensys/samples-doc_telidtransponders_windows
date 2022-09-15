827873fd94f063f23b7f83a6f97075ef0c5d5362
Victor Garcia <vgarcia@microsensys.de>
2022-09-15 08:28:20 +0200

    Updated libraries of console project

--- dotnet/SampleCode_iIDrl_T200_Console/SampleBlocking_VB/Program.vb
+++ dotnet/SampleCode_iIDrl_T200_Console/SampleBlocking_VB/Program.vb
@@ -16,7 +16,7 @@ Module Program
         Console.WriteLine(".NETCore Console")
         Console.WriteLine("SampleAsync_C#")
         Console.WriteLine("--------------------")
-        Console.WriteLine("Library Version: " + iIDReaderLibrary.Version.LibraryVersion)
+        Console.WriteLine("Library Version: " + DocInterfaceControl.LibraryVersion)
 
         'Get DocInterfaceControl instance
         Dim docIntControl As DocInterfaceControl = Console_InitializeDocInterfaceControl()
